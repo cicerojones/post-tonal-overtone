eaa80adf8fcc0216c7004bc16917d1b5ea9d2d09
a <a@as-MacBook-Pro-2.local>
2014-03-14 19:45:30 -0400

    add a fair amount of chord sequencing functionality

--- src/tutorial/core.clj
+++ src/tutorial/core.clj
@@ -13,8 +13,8 @@
 (use 'overtone.live)
 
 (definst foo [] (saw 200))
-
 (foo)
+
 ;; you can stop a particular instrument
 (kill foo)
 
@@ -28,13 +28,11 @@
 ;; default value
 
 (definst bar [freq 220] (saw freq))
-
 (bar)
 (bar 110)
 
 ;; you can also change the amplitude by scaling the signal
 (definst baz [freq 435 amp 0.75] (* amp (saw freq)))
-
 (baz 220)
 (baz 110 0.7)
 
@@ -52,13 +50,10 @@
 
 ;; you can use other kinds of wave-types, such as a sine wave
 (definst quux [freq 440] (* 0.3 (sin-osc freq)))
-
 (quux)
-
+(ctl quux :freq 460) ; CTL needs an argument that is already active
 (map quux [660 770 880 990 1100 1210])
 
-;; CTL needs an argument that is already active
-(ctl quux :freq 460)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -66,6 +61,9 @@
 ;;
 ;;
 ;; here you need to learn about LINE and ASDR
+;;
+;;
+;; you will also make more different kinds of "instruments"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; start modifying the signal with ugens
@@ -111,7 +109,7 @@
 
 ;; 3 seconds of sine, now with an argument for length
 (sin-wave2)
-
+(sin-wave2 220)
 ;; now 5 seconds, with more ADSR modification
 (sin-wave2 :attack 0.1 :sustain 0.15 :release 0.25 :length 5)
 
@@ -224,6 +222,8 @@
 ;;                                        
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; investigate everything going on here to make a bass-drum sound
+;; using a sine wav
 (definst kick [freq 120 dur 0.3 width 0.5]
   (let [freq-env (* freq (env-gen (perc 0 (* 0.99 dur))))
         env (env-gen (perc 0.01 dur) 1 1 0 1 FREE)
@@ -233,7 +233,17 @@
     (compander drum drum 0.2 1 0.1 0.01 0.01)))
 
 (kick)
+(kick 220)
+(kick 100)
 
+;; perc:
+;; [attack 0.01, release 1, level 1, curve -4]
+
+;;   Create a percussive envelope description suitable for 
+;;   use with the env-gen ugen
+
+
+;; compare use of perc and ugens here
 (definst c-hat [amp 0.8 t 0.04]
   (let [env (env-gen (perc 0.001 t) 1 1 0 1 FREE)
         noise (white-noise)
@@ -241,12 +251,16 @@
         filt (bpf (+ sqr noise) 9000 0.5)]
     (* amp env filt)))
 
+(c-hat)
+
+;; what does METRONOME do?
+;; what does setting it equal to a VAR do?
+(now)
+(metro 1) 
 (def metro (metronome 128))
-
 (metro)
-;; => current beat number
 
-(metro 100)
+
 
 ;; created a messed-up version of PLAYER
 (defn tutorial-player [beat]
@@ -260,12 +274,15 @@
 (metro-bpm metro 100)
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; start making atonal chord progressions
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;; pc-set stuff
 
-
-
 (map #(+ 60 %) [0 1 3 4])
-
 (map #(+ (rand-nth [24 36 48 60 72]) %) [0 1 3 4])
 
 (defn voice-rand-set [set-type]
@@ -280,7 +297,11 @@
 (voice-rand-set (rand-nth [*trichords* *tetrachords*]))
 (map baz (map #(midi->hz %) (last (voice-rand-set *pentachords-tn*))))
 
-(map baz (map #(midi->hz %) (voice-and-transpose-rand-set (rand-nth [*trichords* *tetrachords* *pentachords*]) (rand-int 12))))
+(map baz
+     (map #(midi->hz %)
+          (voice-and-transpose-rand-set
+           (rand-nth [*trichords* *tetrachords* *pentachords*])
+           (rand-int 12))))
 
 (defn voice-and-transpose-rand-set [set-type tn-level]
   (let [set (rand-nth set-type)
@@ -307,13 +328,9 @@
 ;;; ADDITIONALLY
 ;; make it possible to sequence these using timing/metronome objects
 
-
-
 ;;; ULTIMATELY
 ;; stringing together sequences of such set|tn|voicing triples should
 ;; be a part of a larger 'compose' function that works with some guidelines
-
-
 
 ;;; SO
 ;; begin by defining a new FN that
@@ -325,7 +342,7 @@
 ;;; the prototypes
 ;; 'the instrument'
 
-(definst baz [freq 440] (* 0.1 (saw freq)))
+(definst baz [freq 440] (* 0.99 (saw freq)))
 
 (baz)
 ;; the 'player'
@@ -342,13 +359,11 @@
 (defn triangle-player []
   (player2 triangle-wave [*trichords* *tetrachords* *pentachords*] 12))
 
+;; must evaluate *variables*
+
 (player2 baz [*trichords* *tetrachords* *pentachords*] 12)
+(triangle-player)
 
-(defn chord-progression-time4 []
-  (let [time (now)]
-    (at time (player2 baz [*trichords* *tetrachords* *pentachords*] 12))))
-
-(chord-progression-time4)
 
 (definst saw100 [] (saw 100))
 (definst saw100v1 [] (* 0.1 (saw 100)))
@@ -366,13 +381,8 @@
 (saw100v2l4noline)
 
 
-(triangle-player)
-;; the 'timing' mechanism
 
 
-(def metro (metronome 128))
-
-(metro)
 
 
 ;;; these belong together
@@ -390,19 +400,41 @@
   (saw-wave (midi->hz m)))
 
 (saw1 60)
-  
-(defn play-chord [a-chord]
+
+
+
+(doseq [note [60 61 62]]
+  (saw2 note))
+
+(defn play-chord-saw [a-chord]
   (doseq [note a-chord] (saw2 note)))
+
+(play-chord-saw [60 61 62])
+
+
+(rand-nth
+ '((80 69 46 73 86) (68 59 48 85 64) (73 63 53 68 58) (68 69 72 87 52) (68 81 46 50 88)))
 
 (defn chord-progression-time []
   (let [time (now)]
-    (at time (noisey2))
+    (at time (play-chord (chord :C4 :major)))
     (at (+ 2000 time) (play-chord (chord :G3 :major)))
     (at (+ 3000 time) (play-chord (chord :F3 :sus4)))
     (at (+ 4300 time) (play-chord (chord :F3 :major)))
     (at (+ 5000 time) (play-chord (chord :G3 :major)))))
 
 (chord-progression-time)
+
+
+(defn chord-progression-time3 []
+  (let [time (now)]
+    (at time (play-chord-saw '(68 81 46 50 88)))
+    (at (+ 2000 time) (play-chord '(80 69 46 73 86)))
+    (at (+ 3000 time) (play-chord '(68 59 48 85 64)))
+    (at (+ 4300 time) (play-chord '(73 63 53 68 58)))
+    (at (+ 5000 time) (play-chord '(68 69 72 87 52)))))
+                                  
+(chord-progression-time3)
 
 (defn chord-progression-time2 []
   (let [time (now)]
@@ -427,34 +459,206 @@
   (let [time (now)]
     (at time (inst :attack 0.15 :sustain 0.2 :release 0.7 :vol 0.5 :length 10))))
 
+(defn chord-progression-time5 [nome]
+  (let [beat (nome)]
+    (at (nome beat) (doseq [note [60 61 62]]
+                      (saw2 note)))
+    (apply-at (nome (inc beat)) chord-progression-time5 nome [])))
+
+(chord-progression-time5 sixty-bpm)
+
+(doseq [note (rand-nth [[60 61 62] [64 66 68]])]
+  (saw2 note))
+
+(defn player2 [inst set-types tn-level-ceiling]
+  (map inst ; instrument
+       (map #(midi->hz %) ; frequency-conversion
+            (voice-and-transpose-rand-set ; voicing
+             (rand-nth set-types) ; set-type
+             (rand-int tn-level-ceiling)))))
+
+(defn voice-and-transpose-rand-set [set-type tn-level]
+  (let [set (rand-nth set-type)
+        voiced-set (map #(+ (rand-nth [36 48 60 72]) %) set)
+        transposed-set (map #(+ tn-level %) voiced-set)
+        set-voicing-group (list set voiced-set tn-level transposed-set)]
+    (do
+      (println set-voicing-group)
+      (last set-voicing-group))))
+
+(doseq [notes (voice-and-transpose-rand-set ; voicing
+               *tetrachords* ; set-type
+               (rand-int 12))]
+  (saw2 notes))
 
 
-;;; modified version
+(voice-and-transpose-rand-set ; voicing
+ *tetrachords* ; set-type
+ (rand-int 12))
+
+(defn chord-progression-time6 [nome]
+  (let [beat (nome)]
+    (at (nome beat)
+        (doseq [notes (voice-and-transpose-rand-set ; voicing
+                       *tetrachords* ; set-type
+                       (rand-int 12))]
+          (saw2 notes)))
+    (apply-at (nome (inc beat)) chord-progression-time5 nome [])))
+
+(chord-progression-time6 sixty-bpm)
 
 
-;; => current beat number
+(defn saw-diss []
+  (doseq [notes (voice-and-transpose-rand-set ; voicing
+                       *tetrachords* ; set-type
+                       (rand-int 12))]
+    (saw2 notes)))
 
-(def metro100 (metronome 100))
-(metro100)
+
+(saw-diss)
+
+(defn saw-diss-print []
+  (doseq [notes (voice-and-transpose-rand-set ; voicing
+                       *tetrachords* ; set-type
+                       (rand-int 12))]
+
+    (saw2 notes)))
+
+(saw-diss-print)
+
+(defn chord-progression-time7 [nome]
+  (let [beat (nome)]
+    (at (nome beat) (saw-diss))
+    (apply-at (nome (inc beat)) chord-progression-time7 nome [])))
+
+(chord-progression-time7 sixty-bpm)
+
+(defn chord-progression-time8 [nome sound]
+  (let [beat (nome)]
+    (at (nome beat) sound)
+    (apply-at (nome (inc beat)) #'chord-progression-time8 nome sound [])))
+
+(chord-progression-time8 sixty-bpm saw-diss)
+
+(definst 1-sec-saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4 length 1] 
+  (* (env-gen (env-lin attack sustain release) 1 1 0 length FREE)
+     (saw freq)
+     vol))
+
+(defn saw2 [music-note]
+  (saw-wave (midi->hz (note music-note))))
+
+(defn chord-progression-time9 [nome]
+  (let [beat (nome)]
+    (at (nome beat) (saw-diss))
+    (apply-at (nome (inc beat)) #'chord-progression-time9 nome [])))
+
+(chord-progression-time9 sixty-bpm)
 
 
-;; created a messed-up version of PLAYER
-(defn player1 [beat]
-  (at (metro beat) (triangle-player))
-  (apply-at (metro (inc beat)) #'player1 (inc beat) []))
 
-(at (metro 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; metronome and sequencing
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(player1 (metro))
+; setup a sound for our metronome to use
 
+(def kick (sample (freesound-path 2086)))
+(def chd1 (play-chord '(60 61 62)))
+
+(chd1)
+(sample (freesound-path 2086))
+  
+
+; setup a tempo for our metronome to use
+(def one-twenty-bpm (metronome 120))
+(def sixty-bpm (metronome 60))
+(one-twenty-bpm)
+
+(sixty-bpm (sixty-bpm))
+
+; this function will play our sound at whatever tempo we've set our metronome to 
+(defn looper [nome sound]    
+    (let [beat (nome)]
+        (at (nome beat) (sound))
+        (apply-at (nome (inc beat)) looper nome sound [])))
+
+; turn on the metronome
+(looper one-twenty-bpm kick)
+(looper one-twenty-bpm triangle-wave)
+(looper one-twenty-bpm (play-chord '(80 69 46 73 86)))
+(stop)
+
+triangle-wave
+(triangle-wave)
+(definst ply-diss [ms 80]
+  (play-chord (list ms)))
+
+; to get a feel for how the metronome works, try defining one at the REPL
+(def nome (metronome 200))
+(nome)
+; 8 
+; why is this 8? shouldn't it be 1? let's try it again
+(nome)
+                                        ;140
+
+; whoah, it's almost like it's ticking in the background. it is, in
+; fact, ticking in the background. a "beat" is just convenient way to
+; represent a timestamp. leave your metronome defined at the REPL, and
+; the beat number will steadily increase, even if you aren't using the
+; object for anything.
 
 ;; ((0 1 2 5 6) (72 61 38 65 78) 8 (80 69 46 73 86))
 ;; ((0 3 4 5 8) (60 51 40 77 56) 8 (68 59 48 85 64))
-((0 2 4 7 9) (72 62 52 67 57) 1 (73 63 53 68 58))
-((0 1 4 7 8) (60 61 64 79 44) 8 (68 69 72 87 52))
-((0 1 2 6 8) (60 73 38 42 80) 8 (68 81 46 50 88))
-((0 3 4 7) (48 63 40 55) 0 (48 63 40 55))
-((0 1 4) (72 37 52) 1 (73 38 53))
+;; ((0 2 4 7 9) (72 62 52 67 57) 1 (73 63 53 68 58))
+;; ((0 1 4 7 8) (60 61 64 79 44) 8 (68 69 72 87 52))
+;; ((0 1 2 6 8) (60 73 38 42 80) 8 (68 81 46 50 88))
+;; ((0 3 4 7) (48 63 40 55) 0 (48 63 40 55))
+;; ((0 1 4) (72 37 52) 1 (73 38 53))
+
+
+;; -------------------------
+;; overtone.live/apply-at
+;; ([ms-time f args* argseq])
+;;   Scheduled function appliction. Works identically to apply, except
+;;    that it takes an additional initial argument: ms-time. If ms-time is
+;;    in the future, function application is delayed until that time, if
+;;    ms-time is in the past function application is immediate.
+
+;;    If you wish to apply slightly before specific time rather than
+;;    exactly at it, see apply-by.
+
+;;    Can be used to implement the 'temporal recursion' pattern. This is
+;;    where a function has a call to apply-at at its tail:
+
+;;    (defn foo
+;;      [t val]
+;;      (println val)
+;;      (let [next-t (+ t 200)]
+;;        (apply-at next-t #'foo [next-t (inc val)])))
+
+;;    (foo (now) 0) ;=> 0, 1, 2, 3...
+
+;;    The fn foo is written in a recursive style, yet the recursion is
+;;    scheduled for application 200ms in the future. By passing a function
+;;    using #'foo syntax instead of the symbole foo, when later called by
+;;    the scheduler it will lookup based on the symbol rather than using
+;;    the instance of the function defined earlier. This allows us to
+;;    redefine foo whilst the temporal recursion is continuing to execute.
+
+;;    To stop an executing temporal recursion pattern, either redefine the
+;;    function to not call itself, or use (stop).
+   
+(defn temporal-recursion
+  [t val]
+  (println val)
+  (let [next-t (+ t 200)]
+    (apply-at next-t #'foo [next-t (inc val)])))
+
+
 
 
 
