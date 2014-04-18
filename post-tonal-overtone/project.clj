6801a6807a730e4196e54d724a15688d9eee821d
a <a@as-MacBook-Pro-2.local>
2014-04-18 14:39:28 -0400

    delete folders

--- post-tonal-overtone/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject tutorial "1.0"
-  :description "overtone tutorialhttps://github.com/overtone/overtone/wiki/I"
-  :url "https://github.com/overtone/overtone/wiki/Installing-overtone"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.9.1"]])
