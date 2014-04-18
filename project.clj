804bbecb85b78de521ef74023895218fb7f4f758
Will Orzo <stevejobsss3000@gmail.com>
2014-04-18 19:41:57 -0400

    start again

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject tutorial "1.0"
-  :description "overtone tutorialhttps://github.com/overtone/overtone/wiki/I"
+(defproject post_tonal_overtone "1.0"
   :url "https://github.com/overtone/overtone/wiki/Installing-overtone"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.9.1"]])
