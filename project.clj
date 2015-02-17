92766e2e1716a250b75fc5d90becff4ac56a2181
Will Orzo <stevejobsss3000@gmail.com>
2015-02-17 13:59:47 -0500

    no, REALLY, update to 1.6."0"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject post_tonal_overtone "1.0"
   :url "https://github.com/overtone/overtone/wiki/Installing-overtone"
-  :dependencies [[org.clojure/clojure "1.6.o"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [overtone "0.9.1"]])
