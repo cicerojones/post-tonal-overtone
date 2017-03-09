9ccf422890b8019032f4f09220ebf5e355f8529c
Will Orzo <stevejobsss3000@gmail.com>
2017-03-09 15:52:20 -0500

    updated cheatsheet may be a problem?

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/java.jdbc "0.5.0"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 ;; [overtone/osc-clj "0.8.1"]
                  [yesql  "0.5.2"]
                  [me.raynes/fs "1.4.6"]
                  [quil "2.2.5"]
