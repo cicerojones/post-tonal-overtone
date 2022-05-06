d462affc7be63aae3ffde969ab3db47558752129
wforzo <stevejobsss3000@gmail.com>
2022-05-06 11:38:16 -0400

    managing and debugging Clojure dependencies is a hassle, let's face it

--- project.clj
+++ project.clj
@@ -1,18 +1,30 @@
-(defproject post_tonal_overtone "1.0"
-  :url "https://github.com/overtone/overtone/wiki/Installing-overtone"
-  :dependencies [[org.clojure/clojure "1.9.0"] ;; stable as of 07/2018
-                 ;; [org.clojure/clojure "1.8.0"]
-                 ;; [org.clojure/clojure "1.9.0-alpha20"]
-                 [overtone "0.10.3"]
-		 [clj-time "0.15.0"] ;; current as of [2018-11-19 Mon]
-                 ;; [org.clojure/math.combinatorics "0.1.1"]
-                 ;; [org.clojure/math.numeric-tower "0.0.4"]
-                 ;; [org.clojure/algo.monads "0.1.5"]
-                 ;; [org.clojure/clojure-contrib "1.2.0"]
-                 ;; [org.clojure/java.jdbc "0.5.0"]
-                 ;; [org.postgresql/postgresql "9.4-1201-jdbc41"]
-                 ;; ;; [overtone/osc-clj "0.8.1"]
-                 ;; [yesql  "0.5.2"]
-                 ;; [me.raynes/fs "1.4.6"]
-                 [quil "2.6.0"]
-                 ])
+ (defproject post_tonal_overtone "1.0"
+  ;;  ;; prefer using deps.edn now?
+  ;; how? comment out stuff here is all?
+  
+  ;; :url "https://github.com/overtone/overtone/wiki/Installing-overtone"
+  :dependencies [
+                 [org.clojure/clojure "1.10.1"];; Stable Release: 1.10.1 (June 6, 2019)
+                 [overtone "0.10.6"]  ;; as of 5/2020
+  ;;                ;; [org.clojure/clojure "1.10.0"] ;; stable as of 07/2018
+                 
+  ;;                ;; [org.clojure/clojure "1.9.0"] ;; stable as of 07/2018
+  ;;                ;; [org.clojure/clojure "1.8.0"]
+  ;;                ;; [org.clojure/clojure "1.9.0-alpha20"]
+                 ;;                ;; [overtone "0.10.3"]
+                 ;; fuck you 
+                 ;; [overtone "0.10.6"] ;; 12/2019 version spits our
+                 ;; synth map
+  ;;       	 [clj-time "0.15.0"] ;; current as of [2018-11-19 Mon]
+  ;;                ;; [org.clojure/math.combinatorics "0.1.1"]
+  ;;                ;; [org.clojure/math.numeric-tower "0.0.4"]
+  ;;                ;; [org.clojure/algo.monads "0.1.5"]
+  ;;                ;; [org.clojure/clojure-contrib "1.2.0"]
+  ;;                ;; [org.clojure/java.jdbc "0.5.0"]
+  ;;                ;; [org.postgresql/postgresql "9.4-1201-jdbc41"]
+  ;;                ;; ;; [overtone/osc-clj "0.8.1"]
+  ;;                ;; [yesql  "0.5.2"]
+  ;;                ;; [me.raynes/fs "1.4.6"]
+                 ;;                [quil "2.6.0"]
+                 ]
+                 )
